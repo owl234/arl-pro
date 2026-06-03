ab152d36af7dbd049ae8c814445adc69506296b8
sienchen <2019020854@stu.sdnu.edu.cn>
2026-06-03 12:56:02 +0800

    - 更改vite.config.js,将 '@' 映射到当前目录下的 'src' 文件夹

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url' // 1. 新增：引入 Node 的 URL 解析模块
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    proxy:{
-      //当你请求 /api/user/login时，Vite会自动帮你把请求转发给后端
-      '/api' :{
+
+  // 2. 新增：配置解析规则
+  resolve: {
+    alias: {
+      // 告诉 Vite：将 '@' 映射到当前目录下的 'src' 文件夹
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+
+  server: {
+    proxy: {
+      // 当你请求 /api/user/login时，Vite会自动帮你把请求转发给后端
+      '/api' : {
         target: 'http://127.0.0.1:5003',
         changeOrigin: true,
         secure: false, // 极其重要：这会忽略ARL本地自签名的HTTPS证书错误。
